8408ef6557c733c060e36f767cfac14edc680449
Alex Rice <rice.alexanderb@gmail.com>
2023-02-02 09:53:58 -0800

    Remove extra lines from file headers

--- Source/UnityAdsAdapter.swift
+++ Source/UnityAdsAdapter.swift
@@ -1,14 +1,7 @@
 // Copyright 2022-2023 Chartboost, Inc.
-// 
+//
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
-
-//
-//  UnityAdsAdapter.swift
-//  ChartboostMediationAdapterUnityAds
-//
-//  Created by Daniel Barros on 10/6/22.
-//
 
 import ChartboostMediationSDK
 import Foundation
