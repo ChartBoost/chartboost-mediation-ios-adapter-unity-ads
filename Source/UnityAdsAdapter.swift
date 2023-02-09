63156f9442ec8e7d1d7b77d28b30c98df5990271
Daniel Barros <daniel.barros@chartboost.com>
2023-02-09 12:47:12 +0100

    Update comments for rebranding

--- Source/UnityAdsAdapter.swift
+++ Source/UnityAdsAdapter.swift
@@ -126,12 +126,12 @@ final class UnityAdsAdapter: NSObject, PartnerAdapter {
         }
     }
     
-    /// Maps a partner setup error to a Helium error code.
-    /// Helium SDK calls this method when a setup completion is called with a partner error.
+    /// Maps a partner setup error to a Chartboost Mediation error code.
+    /// Chartboost Mediation SDK calls this method when a setup completion is called with a partner error.
     ///
     /// A default implementation is provided that returns `nil`.
-    /// Only implement if the partner SDK provides its own list of error codes that can be mapped to Helium's.
-    /// If some case cannot be mapped return `nil` to let Helium choose a default error code.
+    /// Only implement if the partner SDK provides its own list of error codes that can be mapped to Chartboost Mediation's.
+    /// If some case cannot be mapped return `nil` to let Chartboost Mediation choose a default error code.
     func mapSetUpError(_ error: Error) -> ChartboostMediationError.Code? {
         guard let code = UnityAdsInitializationError(rawValue: (error as NSError).code) else {
             return nil
@@ -148,12 +148,12 @@ final class UnityAdsAdapter: NSObject, PartnerAdapter {
         }
     }
     
-    /// Maps a partner load error to a Helium error code.
-    /// Helium SDK calls this method when a load completion is called with a partner error.
+    /// Maps a partner load error to a Chartboost Mediation error code.
+    /// Chartboost Mediation SDK calls this method when a load completion is called with a partner error.
     ///
     /// A default implementation is provided that returns `nil`.
-    /// Only implement if the partner SDK provides its own list of error codes that can be mapped to Helium's.
-    /// If some case cannot be mapped return `nil` to let Helium choose a default error code.
+    /// Only implement if the partner SDK provides its own list of error codes that can be mapped to Chartboost Mediation's.
+    /// If some case cannot be mapped return `nil` to let Chartboost Mediation choose a default error code.
     func mapLoadError(_ error: Error) -> ChartboostMediationError.Code? {
         if let error = error as? UADSBannerError {
             // Banner error code
@@ -190,12 +190,12 @@ final class UnityAdsAdapter: NSObject, PartnerAdapter {
         }
     }
     
-    /// Maps a partner show error to a Helium error code.
-    /// Helium SDK calls this method when a show completion is called with a partner error.
+    /// Maps a partner show error to a Chartboost Mediation error code.
+    /// Chartboost Mediation SDK calls this method when a show completion is called with a partner error.
     ///
     /// A default implementation is provided that returns `nil`.
-    /// Only implement if the partner SDK provides its own list of error codes that can be mapped to Helium's.
-    /// If some case cannot be mapped return `nil` to let Helium choose a default error code.
+    /// Only implement if the partner SDK provides its own list of error codes that can be mapped to Chartboost Mediation's.
+    /// If some case cannot be mapped return `nil` to let Chartboost Mediation choose a default error code.
     func mapShowError(_ error: Error) -> ChartboostMediationError.Code? {
         guard let code = UnityAdsShowError(rawValue: (error as NSError).code) else {
             return nil
