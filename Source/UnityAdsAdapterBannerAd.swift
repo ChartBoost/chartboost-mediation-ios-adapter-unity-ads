d03a9ac99412a46cf8d60b87390eef9ccc294242
Daniel Barros <daniel.barros@chartboost.com>
2024-04-18 10:02:36 +0530

    Update show method

--- Source/UnityAdsAdapterBannerAd.swift
+++ Source/UnityAdsAdapterBannerAd.swift
@@ -43,14 +43,6 @@ final class UnityAdsAdapterBannerAd: UnityAdsAdapterAd, PartnerAd {
         // Load it
         banner.load()
     }
-    
-    /// Shows a loaded ad.
-    /// It will never get called for banner ads. You may leave the implementation blank for that ad format.
-    /// - parameter viewController: The view controller on which the ad will be presented on.
-    /// - parameter completion: Closure to be performed once the ad has been shown.
-    func show(with viewController: UIViewController, completion: @escaping (Result<PartnerDetails, Error>) -> Void) {
-        // no-op
-    }
 }
 
 extension UnityAdsAdapterBannerAd: UADSBannerViewDelegate {
